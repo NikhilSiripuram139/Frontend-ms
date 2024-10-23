3bb203c950eb7bff945abc606d535c6418fdef2b
NikhilSiripuram139 <siripuramnikhil139@gmail.com>
2024-10-23 15:52:04 +0530

    Initial commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,15 @@
+export const environment = {
+    production: false,
+
+    myurl : 'http://localhost:4200',
+    api : 'http://localhost:3000',
+
+    CLIENT_ID : process.env["CLIENT_ID"],
+    OBJECT_ID : process.env['OBJECT_ID'],
+    TENANT_ID : process.env['TENANT_ID'],
+
+    REDIRECT_URI : process.env['REDIRECT_URI'],
+
+    CLIENT_SECRET : process.env['CLIENT_SECRET'],
+    CLIENT_SECRET_ID : process.env['CLIENT_SECRET_ID'],
+}
